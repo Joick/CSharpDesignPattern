5bd9a130b6a2f90623fe342977b236ceed272aef
Joick <348606155@qq.com>
2020-06-17 17:29:51 +0800

    feat: 添加单例模式

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = SingletonSample.GetInstance();
+            instance?.Print();
+            Console.WriteLine("exit");
+        }
+    }
+}
---
+++ SingletonPattern/SingletonSample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// 单例模式类
+    /// </summary>
+    public class SingletonSample
+    {
+        /// <summary>
+        /// 静态变量存储类的实例
+        /// </summary>
+        private static SingletonSample _instance;
+
+        ///// <summary>
+        ///// 如果出现多线程情况,考虑使用volatile关键字
+        ///// </summary>
+        //private static volatile SingletonSample _instance;
+
+        /// <summary>
+        /// 锁
+        /// </summary>
+        private static readonly object InstanceLocker = new object();
+
+        /// <summary>
+        /// 私有构造函数, 使得类无法在外部被实例化
+        /// </summary>
+        private SingletonSample() { }
+
+        /// <summary>
+        /// 使用方法获取单例实例
+        /// </summary>
+        /// <returns></returns>
+        public static SingletonSample GetInstance()
+        {
+            if (_instance != null)
+                return _instance;
+
+            lock (InstanceLocker)
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonSample();
+                }
+            }
+
+            return _instance;
+        }
+
+        /// <summary>
+        /// 调用方法
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine("print");
+        }
+    }
+}
